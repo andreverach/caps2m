8b9e35886c3f9ca52a8742de13a9a9841b65d0d2
André <andrevera.pe@gmail.com>
2025-12-03 17:21:43 -0500

    seo implementado y prettier

--- js/main.js
+++ js/main.js
@@ -1,48 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // Navbar Elements
-  const navbar = document.getElementById("navbar");
-  const mobileMenuBtn = document.getElementById("mobile-menu-btn");
-  const mobileMenu = document.getElementById("mobile-menu");
+  const navbar = document.getElementById('navbar')
+  const mobileMenuBtn = document.getElementById('mobile-menu-btn')
+  const mobileMenu = document.getElementById('mobile-menu')
 
   // Scroll to Top Button Elements
-  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn')
 
   // Navbar Scroll Effect
-  window.addEventListener("scroll", () => {
+  window.addEventListener('scroll', () => {
     if (window.scrollY > 10) {
-      navbar.classList.add("shadow-md");
+      navbar.classList.add('shadow-md')
     } else {
-      navbar.classList.remove("shadow-md");
+      navbar.classList.remove('shadow-md')
     }
 
     // Scroll to Top Button Visibility
     if (window.scrollY > 300) {
-      scrollToTopBtn.classList.remove(
-        "opacity-0",
-        "invisible",
-        "translate-y-10"
-      );
-      scrollToTopBtn.classList.add("opacity-100", "visible", "translate-y-0");
+      scrollToTopBtn.classList.remove('opacity-0', 'invisible', 'translate-y-10')
+      scrollToTopBtn.classList.add('opacity-100', 'visible', 'translate-y-0')
     } else {
-      scrollToTopBtn.classList.add("opacity-0", "invisible", "translate-y-10");
-      scrollToTopBtn.classList.remove(
-        "opacity-100",
-        "visible",
-        "translate-y-0"
-      );
+      scrollToTopBtn.classList.add('opacity-0', 'invisible', 'translate-y-10')
+      scrollToTopBtn.classList.remove('opacity-100', 'visible', 'translate-y-0')
     }
-  });
+  })
 
   // Mobile Menu Toggle
-  mobileMenuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-  });
+  mobileMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden')
+  })
 
   // Scroll to Top Action
-  scrollToTopBtn.addEventListener("click", () => {
+  scrollToTopBtn.addEventListener('click', () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
-    });
-  });
-});
+      behavior: 'smooth'
+    })
+  })
+})
